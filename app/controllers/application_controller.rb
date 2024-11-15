03e9315954b037b21d6fafc3d562f2cd24963e6f
petebarbosa <pedrovbb89@gmail.com>
2024-11-15 04:31:11 -0300

    feat: wraps all devise pages into turbo frames

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
-  before_action :configure_permitted_paramenters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   protected
 
-  def configure_permitted_paramenters
-    devise_paramenter_sanitizer.permit(:sign_up, keys: %i[first_name last_name terms_and_conditions])
-    devise_paramenter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name terms_and_conditions])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
   end
 end
