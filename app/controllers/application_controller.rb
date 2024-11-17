a28a52dd9058ddbd01210e19ed9e95902ae5a5dc
petebarbosa <pedrovbb89@gmail.com>
2024-11-17 16:38:51 -0300

    chore: remove terms_and_condidtions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name terms_and_conditions])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
   end
 end
